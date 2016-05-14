60e799b319b5021993cb809fd8b9cbd915085bd2
r57zone <r57zone@gmail.com>
2016-05-14 20:22:57 +0400

    update

--- Project1.dpr
+++ Project1.dpr
@@ -5,14 +5,14 @@ program Project1;
 uses Windows, ShlObj, ShellAPI;
 
 function GetSpecialPath(CSIDL: word): string;
-var s: string;
+  var 
+  s: string;
 begin
-SetLength(s, MAX_PATH);
-if not SHGetSpecialFolderPath(0, PChar(s), CSIDL, true)
-then s := '';
-result := PChar(s);
+  SetLength(s, MAX_PATH);
+  if not SHGetSpecialFolderPath(0, PChar(s), CSIDL, true) then s:='';
+  Result:=PChar(s);
 end;
 
 begin
-ShellExecute(0,'open','cmd.exe',Pchar('/c rundll32 "'+GetSpecialPath($26)+'\Windows Photo Viewer\PhotoViewer.dll",ImageView_Fullscreen '+ParamStr(1)),nil,SW_HIDE);
+  ShellExecute(0,'open','cmd.exe',Pchar('/c rundll32 "'+GetSpecialPath($26)+'\Windows Photo Viewer\PhotoViewer.dll",ImageView_Fullscreen '+ParamStr(1)),nil,SW_HIDE);
 end.
